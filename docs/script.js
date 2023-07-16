f4c59f42a7cd7537d88a94a5b20b70ef9e95cde6
MoTheMelon <ortiz97mi@gmail.com>
2023-07-16 12:20:25 -0400

    commit

---
+++ docs/script.js
@@ -0,0 +1,12 @@
+
+function showLine (element){
+    var line = Rect();
+}
+function hideLine (element){
+    var line = Rect();
+}
+
+const canvas = document.getElementById("drawing-canvas");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0,0,150,75);
